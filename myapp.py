18dd3fc89215666dd277e6d6eb739f5347c72699
SarwechAbro <SarwechAbro>
2024-07-02 19:11:25 +0500

    first comment added

---
+++ myapp.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+students = { 
+    'sarwech':{ 
+        'name': 'Sarwech Imdad',
+        'id':'BSE-22F-138', 
+        'Department': 'software enginnering', 
+        'section':'SE4C'
+    },
+    'asim':{ 'name':'Asim Hayat',
+    'id':'BSE-22F-132', 
+    'Department': 'software enginnering', 
+    'section':'SE4C'
+    },
+    'abrar':{ 'name':'abrar',
+    'id':'BSE-22F-128', 
+    'Department': 'software enginnering', 
+    'section':'SE4C'},
+    }
+ 
+@app.get("/api/{student_name}")
+def index(student_name:str):
+    if student_name in students:
+        return {'student': students[student_name]}
+        
+    else:
+        return {'students': students}
