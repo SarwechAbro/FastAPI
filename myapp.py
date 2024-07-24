e9d80cbf7bc6df4f73743d7d00566f4661e9d461
SarwechAbro <SarwechAbro>
2024-07-24 12:03:25 +0500

    changes

--- myapp.py
+++ myapp.py
@@ -8,24 +8,24 @@ students = {
     'sarwech':{ 
         'name': 'Sarwech Imdad',
         'id':'BSE-22F-138', 
-        'Department': 'software enginnering', 
+        'Department': 'software engineering', 
         'section':'SE4C'
     },
     'asim':{ 'name':'Asim Hayat',
     'id':'BSE-22F-132', 
-    'Department': 'software enginnering', 
+    'Department': 'software engineering', 
     'section':'SE4C'
     },
     'abrar':{ 'name':'abrar',
     'id':'BSE-22F-128', 
-    'Department': 'software enginnering', 
+    'Department': 'software engineering', 
     'section':'SE4C'},
     }
  
 @app.get("/api/{student_name}")
 def index(student_name:str):
     if student_name in students:
-        return {'student': students[student_name]}
+        return {'student': students[student_name]['name']}
         
     else:
         return {'students': students}
